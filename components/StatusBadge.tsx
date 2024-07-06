e04aa96a77b4aba587f8578bcf8c8f4c35891d33
Mayank Mishra <117895206+mishraji900@users.noreply.github.com>
2024-07-06 17:05:26 +0530

    first commit

---
+++ components/StatusBadge.tsx
@@ -0,0 +1,33 @@
+import clsx from "clsx";
+import Image from "next/image";
+
+import { StatusIcon } from "@/constants";
+
+export const StatusBadge = ({ status }: { status: Status }) => {
+  return (
+    <div
+      className={clsx("status-badge", {
+        "bg-cyan-600": status === "scheduled",
+        "bg-blue-600": status === "pending",
+        "bg-red-600": status === "cancelled",
+      })}
+    >
+      <Image
+        src={StatusIcon[status]}
+        alt="doctor"
+        width={24}
+        height={24}
+        className="h-fit w-3"
+      />
+      <p
+        className={clsx("text-12-semibold capitalize", {
+          "text-cyan-500": status === "scheduled",
+          "text-amber-500": status === "pending",
+          "text-red-500": status === "cancelled",
+        })}
+      >
+        {status}
+      </p>
+    </div>
+  );
+};
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://5bdd6d51d329eb5a8129ffdc8b6ad9c8@o4507554251014144.ingest.de.sentry.io/4507554253570128",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
